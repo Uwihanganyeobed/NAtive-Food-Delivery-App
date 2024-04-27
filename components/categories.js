1fe5a1a111bc2fd5f01c173c7ce3115f4c8a3e2e
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-27 08:08:57 +0200

    categories

--- components/categories.js
+++ components/categories.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { categories } from '../constants'
 
 export default function Categories() {
+  const [activeCategory, setActiveCategory] = useState(null);
   return (
-    <View>
-      <Text>categories</Text>
+    <View className='mt-4'>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className='overflow-visible'
+        contentContainerStyle={{
+          paddingHorizontal: 40,
+          gap: 20
+        }} >
+        {
+          categories.map((category, index) => {
+            let isActive = category.id == activeCategory;
+            let btnClass = isActive ? ' bg-gray-600' : ' bg-gray-200 ';
+            let textClass = isActive ? ' font-semibold text-gray-800 ' : ' text-gray-500 ';
+            return (
+              <View key={index} className='flex justify-center items-center mr-6'>
+                <TouchableOpacity
+                  onPress={() => setActiveCategory(category.id)}
+                  className={'p-1 rounded-full shadow bg-gray-200 ' + btnClass}>
+                  <Image style={{ width: 45, height: 45 }} source={category.image} />
+                </TouchableOpacity>
+                <Text className={'text-sm' + textClass}>{category.name}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
     </View>
   )
 }
