f612fb4e6c4b517f4604837b1d1e80e0e54a515e
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-27 07:38:36 +0200

    products

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,140 @@
-export const categories=[
-   {id: 1,
-   name: 'Pizza',
-image: require('../assets/images/')}
-]
+export const categories = [
+  {
+    id: 1,
+    name: "Pizza",
+    image: require("../assets/images/icons8-pizza-96.png"),
+  },
+  {
+    id: 2,
+    name: "Burger",
+    image: require("../assets/images/icons8-pizza-96.png"),
+  },
+  {
+    id: 3,
+    name: "Italian",
+    image: require("../assets/images/icons8-pizza-96.png"),
+  },
+  {
+    id: 4,
+    name: "Chinese",
+    image: require("../assets/images/icons8-pizza-96.png"),
+  },
+  {
+    id: 5,
+    name: "Noodles",
+    image: require("../assets/images/icons8-pizza-96.png"),
+  },
+];
+
+export const featured={
+   id: 1,
+   title: "Hot and Spicy",
+   description: 'soft and tender fried chicken',
+   restaurant: [
+      {
+         id: 1,
+         name: 'Papa Johns',
+         image: require('../assets/images/icons8-pizza-96.png'),
+         description: 'Hot and Spicy Pizzas',
+         lng: 38.2145602,
+         lat: -85.53244269,
+         address: '434 second street',
+         starts: 4,
+         reviews: '4.4k',
+         category: 'Fast Food',
+         dishes: [
+            {
+               id: 1,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            },
+            {
+               id: 2,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            },
+            {
+               id: 3,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            }
+         ]
+      },
+      {
+         id: 2,
+         name: 'Papa Johns',
+         image: require('../assets/images/icons8-pizza-96.png'),
+         description: 'Hot and Spicy Pizzas',
+         lng: 38.2145602,
+         lat: -85.53244269,
+         address: '434 second street',
+         starts: 4,
+         reviews: '4.4k',
+         category: 'Fast Food',
+         dishes: [
+            {
+               id: 1,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            },
+            {
+               id: 2,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            },
+            {
+               id: 3,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            }
+         ]
+      },
+      {
+         id: 3,
+         name: 'Papa Johns',
+         image: require('../assets/images/icons8-pizza-96.png'),
+         description: 'Hot and Spicy Pizzas',
+         lng: 38.2145602,
+         lat: -85.53244269,
+         address: '434 second street',
+         starts: 4,
+         reviews: '4.4k',
+         category: 'Fast Food',
+         dishes: [
+            {
+               id: 1,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            },
+            {
+               id: 2,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            },
+            {
+               id: 3,
+               name: 'Pizza',
+               description: 'Cheezy garlic pizza',
+               price: 10,
+               image: require('../assets/images/icons8-cupcake-emoji-96.png')
+            }
+         ]
+      },
+   ]
+}
