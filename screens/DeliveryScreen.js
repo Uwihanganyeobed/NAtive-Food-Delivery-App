e9d574ded884c7745c7bb1842583131cd07ce82d
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-27 11:34:13 +0200

    deliver

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,33 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { featured } from '../constants'
-import { useNavigation } from '@react-navigation/native';
-import MapView, {Marker} from 'react-native-maps';
-import { themeColors } from '../theme';
+import { View,Image, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import * as Icon from 'react-native-feather';
+import { featured } from "../constants";
+import { useNavigation } from "@react-navigation/native";
+import MapView, { Marker } from "react-native-maps";
+import { themeColors } from "../theme";
 export default function DeliveryScreen() {
-   const restaurant=featured.restaurant[0];
-   const navigation=useNavigation();
+  const restaurant = featured.restaurant[0];
+  const navigation = useNavigation();
   return (
-    <View className='flex-1'>
+    <View className="flex-1">
       {/* map view */}
       <MapView
-      initialRegion={{
-         latitude: restaurant.lat,
-         longitude: restaurant.lng,
-         latitudeDelta: 0.01,
-         longitudeDelta: 0.01
-      }}
-      className='flex-1'
-      mapType='standard'>
-         <Marker coordinate={{
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        className="flex-1"
+        mapType="standard"
+      >
+        <Marker
+          coordinate={{
             latitude: restaurant.lat,
             longitude: restaurant.lng,
-         }}
-         title={restaurant.name}
-         description={restaurant.description}
-         pinColor={themeColors.bgColor(1)}
-         />
+          }}
+          title={restaurant.name}
+          description={restaurant.description}
+          pinColor={themeColors.bgColor(1)}
+        />
       </MapView>
+      <View className="rounded-t-3xl -mt-12 bg-white relative">
+        <View className="flex-row justify-between px-5 pt-10">
+         <View>
+         <Text className="text-lg text-gray-700 font-semibold">
+            Estimated Arrival
+          </Text>
+          <Text className="text-3xl font-extrabold text-gray-700">
+            20-30 Minutes
+          </Text>
+          <Text className="mt-2 text-gray-700 font-semibold">
+            Your Order is on its way
+          </Text>
+         </View>
+         <Image className='w-24 h-24'source={require('../assets/images/fast-food-delivery-12982.png')} />
+        </View>
+        <View
+         style={{backgroundColor: themeColors.bgColor(0.8)}}
+         className='p-2 flex-row justify-center items-center rounded-full my-5 mx-2'>
+         <View className='p-1 rounded-full'style={{backgroundColor: 'rbga(255,255,255,0.4)'}}>
+            <Image className='h-16 w-16 rounded-full'source={require('../assets/images/user.png')}/>
+
+         </View>
+         <View className='flex-1'>
+            <Text className='text-lg font-bold text-white'>Major Vargas</Text>
+            <Text className='text-lg font-semibold text-white'>Your Rider</Text>
+         </View>
+         <View className='flex-row items-center space-x-3 mr-3'>
+            <TouchableOpacity className='bg-white p-2 rounded-full'>
+               <Icon.Phone fill={themeColors.bgColor(1)} strokeWidth={1} stroke={themeColors.bgColor(1)} />
+            </TouchableOpacity>
+            <TouchableOpacity
+            onPress={()=>navigation.navigate('Home')}
+            className='bg-white p-2 rounded-full'>
+               <Icon.X stroke={'red'} strokeWidth={3} />
+            </TouchableOpacity>
+         </View>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
