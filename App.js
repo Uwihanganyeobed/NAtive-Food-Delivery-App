3751ed746dc080c9d56aad3096db4d82fd452221
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-28 17:04:44 +0200

    adding redux

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
-
+import { Provider } from 'react-redux';
+import { store } from './store';
 export default function App() {
   return (
+    <Provider store={store}>
     <Navigation />
+    </Provider>
   );
 }
