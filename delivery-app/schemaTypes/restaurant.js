55749fb3273afaebe732e8806170856300ec581d
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-29 12:55:50 +0200

    adding restaurant

--- delivery-app/schemaTypes/restaurant.js
+++ delivery-app/schemaTypes/restaurant.js
@@ -1,31 +1,67 @@
 // import {defineField, defineType} from 'sanity'
 
 export default ({
-  name: 'dish',
-  title: 'Dishes',
+  name: 'restaurant',
+  title: 'Restaurant',
   type: 'document',
   fields: [
    {
     name: 'name',
     type: 'string',
-    title: 'Dish Name',
+    title: 'Name',
     validation: rule=>rule.required()
    },
    {
     name: 'description',
     type: 'string',
-    title: 'Dish  Description',
+    title: 'Description',
     validation: rule=>rule.required()
    },
    {
     name: 'image',
     type: 'image',
-    title: 'image of the category',
+    title: 'image of the restaurant',
    },
    {
-      name: 'price',
+      name: 'lat',
       type: 'number',
-      title: 'price of the dish in USD',
+      title: 'latitude of the restaurant',
+     },
+     {
+      name: 'lng',
+      type: 'number',
+      title: 'longitude of the restaurant',
+     },
+     {
+      name: 'address',
+      type: 'string',
+      title: 'Restaurant Adress',
+      validation: rule=>rule.required(),
+     },
+     {
+      name: 'rating',
+      type: 'number',
+      title: 'Enter a number between 1 and 5',
+      validation: rule=>rule.required().min(1).max(5).error('Please enter a value between 1 and 5')
      }
+     ,
+     {
+      name: 'reviews',
+      type: 'string',
+      title: 'Reviews',
+     },
+     {
+      name: 'type',
+      title: 'Category',
+      validation: rule=>rule.required(),
+      type: 'reference',
+      to: [{type: 'category'}]
+     },
+     {
+      name: 'dishes',
+      type: 'array',
+      title: 'Dishes',
+      of: [{type: 'reference', to: [{type: 'dish'}]}]
+     },
   ],
 })
