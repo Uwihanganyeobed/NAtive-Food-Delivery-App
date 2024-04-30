3099eae5d107bc44252f619f3fae3487151322c7
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-30 07:52:46 +0200

    api

---
+++ api.js
@@ -0,0 +1,38 @@
+import  {sanityClient}  from "@sanity/client";
+let sanityQuery= (query, params)=> sanityClient.fetch(query,params);
+
+export const getFeaturedRestaurants=()=>{
+   return sanityQuery(`
+   *[_type== 'featured']{
+      ...,
+      restaurants[]->{
+        ...,
+        dishes[]->{
+    ...
+      },
+        type->{
+        name
+        }
+      }
+    }
+   `)
+}
+
+export const getCategories=()=>{
+   return sanityQuery(`
+   *[_type== 'categories']
+   `)
+}
+export const getFeaturedRestaurantById=id=>{
+   return sanityQuery(`
+   *[_type== 'featured' && _id== '$id]{
+      ...,
+      restaurants[]->{
+         ...,
+         dishes[]->{
+            type->{ name  }
+         }
+      }
+   }[0]
+   `, {id})
+}
